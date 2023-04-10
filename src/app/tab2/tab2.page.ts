369874f9941ad9961e2aa69362b8a49ebd6e726f
Kevin <playsbykevin@gmail.com>
2023-04-10 17:02:36 +0300

    fotode salvestamine v2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { dummyImage } from './image.const';
+import { Storage } from '@ionic/storage-angular';
+
 import {
   Camera,
   CameraResultType,
@@ -19,7 +21,17 @@ export class Tab2Page {
       `data:image/png;base64, ${dummyImage}`
     );
 
-  constructor(private sanitizer: DomSanitizer) {}
+  images: string[]  = [];
+
+  constructor(private sanitizer: DomSanitizer, private storage: Storage) {}
+
+  async ngOnInit() {
+    await this.storage.create();
+    const photos = await this.storage.get('photos');
+    if (photos) {
+      this.images = JSON.parse(photos);
+    }
+  }
 
   takePhoto = async () => {
     const result = await Camera.requestPermissions();
@@ -32,13 +44,16 @@ export class Tab2Page {
       });
 
       console.log('base64:', photoBase64);
-      this.imageSource = this.sanitizer.bypassSecurityTrustResourceUrl(
-        `data:image/png;base64, ${photoBase64.base64String || dummyImage}`
-      );
+      this.images.push(<string>this.sanitizer.bypassSecurityTrustResourceUrl(
+        `data:image/png;base64, ${photoBase64.base64String}`
+      ));
+      this.onPhotosUpdated();
     } else {
       console.error('No access to camera');
     }
-
-
   };
+
+  onPhotosUpdated() {
+    this.storage.set('photos', JSON.stringify(this.images));
+  }
 }
