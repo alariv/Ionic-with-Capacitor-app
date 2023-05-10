bb1e7d698ba2fbb732c4ebb0612d84aa865c549e
Alari Verev <alariverev36@gmail.com>
2023-05-10 11:29:35 +0300

    rest get

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { ARLauncherPage } from '../ar-launcher/ar-launcher.page';
 // import RouterOutlet from 'ion-router-outlet';
 
 @Component({
@@ -10,21 +8,84 @@ import { ARLauncherPage } from '../ar-launcher/ar-launcher.page';
   encapsulation: ViewEncapsulation.None,
 })
 export class Tab3Page {
-  constructor(private modalCtrl: ModalController) {}
+  constructor() {}
 
   ngOnInit() {}
+  initialForecast = {
+    location: {
+      name: '',
+      region: '',
+      country: '',
+      lat: 59.43,
+      lon: 24.73,
+      tz_id: '',
+      localtime_epoch: 1682281899,
+      localtime: '',
+    },
+    forecast: {
+      forecastday: [
+        {
+          date: '',
+          date_epoch: 1681689600,
+          day: {
+            maxtemp_c: 10.7,
+            maxtemp_f: 51.3,
+            mintemp_c: 1.7,
+            mintemp_f: 35.1,
+            avgtemp_c: 5.5,
+            avgtemp_f: 41.9,
+            maxwind_mph: 8.1,
+            maxwind_kph: 13,
+            totalprecip_mm: 0,
+            totalprecip_in: 0,
+            avgvis_km: 10,
+            avgvis_miles: 6,
+            avghumidity: 63,
+            condition: {
+              text: '',
+              icon: '//cdn.weatherapi.com/weather/64x64/day/113.png',
+              code: 1000,
+            },
+            uv: 4,
+          },
+        },
+      ],
+    },
+  };
+  result: any = {};
 
-  async openAR() {
-    const modal: HTMLIonModalElement = await this.modalCtrl.create({
-      component: ARLauncherPage,
-      swipeToClose: true,
-      // presentingElement: this.routerOutlet.nativeEl,
-    });
+  fetchLastTenDays = async () => {
+    console.log('fetchLastTenDays');
+    const endDate = new Date(new Date().getTime() - 1 * (1000 * 60 * 60 * 24));
+    const startDate = new Date(
+      new Date().getTime() - 7 * (1000 * 60 * 60 * 24)
+    );
+    const url = `https://weatherapi-com.p.rapidapi.com/history.json?q=Tallinn&dt=${startDate.getFullYear()}-${
+      startDate.getMonth() + 1
+    }-${startDate.getDate()}&end_dt=${endDate.getFullYear()}-${
+      endDate.getMonth() + 1
+    }-${endDate.getDate()}&lang=en`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/octet-stream',
+        'X-RapidAPI-Key': 'ba0731616fmshccf9f6fe11502a1p1f4626jsna2eccfca6768',
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+      },
+    };
 
-    return await modal.present();
-  }
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+      this.result = result;
+      return result || {};
+    } catch (error) {
+      console.error(error);
+      this.result = error || {};
+      return error;
+    }
+  };
 
-  close() {
-    this.modalCtrl.dismiss();
-  }
+  lastTenDays = this.fetchLastTenDays();
 }
