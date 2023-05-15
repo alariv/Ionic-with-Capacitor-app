fc13f2d243112ee8aae18be984ec9edf38a9ef39
Kevin <playsbykevin@gmail.com>
2023-05-15 18:40:22 +0300

    Soovitused eraldi failis

---
+++ src/app/tab1/suggestions.ts
@@ -0,0 +1,75 @@
+
+export const UVSuggestion = {
+  "minimal": {
+    title: "UV index is minimal",
+    subtitle: "Nothing to worry about!",
+  },
+  "low": {
+    title: "UV index is low",
+    subtitle: "Wear a hat and/or sunglasses to protect your eyes.",
+  },
+  "moderate": {
+    title: "UV index is moderate",
+    subtitle: "Sunglasses and a hat is recommended. Use sunscreen with an SPF of at least 30.",
+  },
+  "high": {
+    title: "UV index is high",
+    subtitle: "Minimize sun exposure during midday(10am - 4pm). Try to stay in shade when outside. SPF with at least 30 sunscreen is a must.",
+  },
+  "extreme": {
+    title: "UV index is extremely high",
+    subtitle: "Apply sunscreen of SPF 30 or above every 2 hours or you'll burn in less than 5 minutes. The sun is a deadly laser!",
+  }
+}
+
+export const ConditionSuggestion = {
+  snow: {
+    title: "It's snowing!",
+    subtitle: "If going outside, wear multiple layers of clothing that can protect you from the cold.",
+  },
+  cloudy: {
+    title: "It's cloudy.",
+    subtitle: "Take a jacket with you!",
+  },
+  overcast: {
+    title: "There's overcast.",
+    subtitle: "Refer to outside temperature for clothing.",
+  },
+  sunny: {
+    title: "It's sunny!",
+    subtitle: "Wear headwear such as hats to protect from direct sunlight. Check UV index!",
+  },
+  thunderstorm: {
+    title: "There is a thunderstorm!",
+    subtitle: "Avoid going outside or if you do, avoid powerlines, bodies of water and standing under trees.",
+  },
+  rain: {
+    title: "It's raining.",
+    subtitle: "Take an umbrella with you or wear water-resistant clothing.",
+  },
+  clear: {
+    title: "Clear skies!",
+    subtitle: "Enjoy the nice weather, with clouds nowhere in sight.",
+  }
+}
+
+export const WeatherImage = {
+  day: {
+    sunny: "assets/images/sun/26.png",
+    cloudy: "assets/images/sun/27.png",
+    overcast: "assets/images/cloud/33.png",
+    snow: "assets/images/cloud/23.png",
+    thunderstorm: "assets/images/sun/16.png",
+    rain: "assets/images/sun/8.png",
+    clear: "assets/images/sun/6.png",
+  },
+  night: {
+    sunny: "assets/images/moon/10.png",
+    cloudy: "assets/images/moon/15.png",
+    overcast: "assets/images/cloud/32.png",
+    snow: "assets/images/cloud/23.png",
+    thunderstorm: "assets/images/moon/20.png",
+    rain: "assets/images/moon/1.png",
+    clear: "assets/images/moon/2.2.png",
+  }
+}
